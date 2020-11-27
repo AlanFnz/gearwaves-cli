92e01784fc34cc4bfe1b29dae32fe804822a2230
Alan Fernandez <alan.f@msn.com>
2020-11-27 16:18:07 -0300

    First commit. Gearwaves Client

---
+++ src/components/layout/Nav.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import React from 'react';
+// Bootstrap
+import Navbar from 'react-bootstrap/Navbar';
+
+const Nav = () => {
+  return ( 
+    <Navbar bg="dark" variant="dark">
+      <p>ALL PRODUCTS</p>
+    </Navbar>
+  )
+};
+
+export default Nav;
