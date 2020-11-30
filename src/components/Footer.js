625bca696f49942d9aa766f75fbdb148065448c9
Alan Fernandez <alan.f@msn.com>
2020-11-30 19:04:09 -0300

    Tweaking styles and responsiveness of components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,13 +10,15 @@ const Footer = () => {
       <div className='footer__logo'>
         <img src='/img/logo-green.png' alt='Logo'></img>
       </div>
-      <ul className='footer__nav'>
-        <li><a href='/#'>About us</a></li>
-        <li><a href='/#'>Download apps</a></li>
-        <li><a href='/#'>Become a guide</a></li>
-        <li><a href='/#'>Careers</a></li>
-        <li><a href='/#'>Contact</a></li>
-      </ul>
+      <div>
+        <ul className='footer__nav'>
+          <li><a href='/#'>About us</a></li>
+          <li><a href='/#'>Download apps</a></li>
+          <li><a href='/#'>Become a guide</a></li>
+          <li><a href='/#'>Careers</a></li>
+          <li><a href='/#'>Contact</a></li>
+        </ul>
+      </div>
       <p className='footer__copyright'>&copy; by Alan Fernandez. All rights reserved.</p>
     </footer>
   )
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,9 @@
 import React, { Fragment } from 'react';
 // Bootstrap
 // import { Navbar } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 // Styles
 import '../styles/Nav.css';
-
-
 
 const Nav = () => {
   const user = false;
@@ -12,30 +11,44 @@ const Nav = () => {
   const loginSignupMarkup = 
     user ? ( 
       <Fragment>
-        <a className='nav__el nav__el--logout' href='/#'>Logout</a>
+        <a className='nav__el' href='/#'>Logout</a>
         <a className='nav__el' href='/me'>
-          <img className='nav__user-img' src='img/users/default.jpg' alt='User' />
-          <span>User name</span>
+          <img className='nav__user-img d-none d-sm-block' src='img/users/default.jpg' alt='User' />
+          <span className='d-none d-md-block'>User name</span>
         </a>
       </Fragment>
     ) : (
-      <Fragment>
-        <a className='nav__el' href='/login'>Log in</a>
-        <a className='nav__el nav__el--cta' href='/#'>Sign up</a>
-      </Fragment>
+      <Row xs='2' className='nav__el--signup'>
+        <Col>
+          <a className='nav__el' href='/login'>Log in</a>
+        </Col>
+        <Col>
+          <a className='nav__el nav__el--cta' href='/#'>Sign up</a>
+        </Col>
+      </Row>
     );
 
   return ( 
     <header className='header'>
-      <nav className='nav nav--products'>
-        <a className='nav__el' href='/'>ALL PRODUCTS</a>
-      </nav>
-      <div className='header__logo'>
-        <img src='/img/logo-white.png' alt='logo' />
-      </div>
-      <nav className='nav nav--user'>
-        {loginSignupMarkup}
-      </nav>
+      <Container>
+        <Row xs='2' sm='3'>
+          <Col className='d-none d-sm-block'>
+            <nav className='nav nav--products'>
+              <a className='nav__el' href='/'>ALL PRODUCTS</a>
+            </nav>
+          </Col>
+          <Col>
+            <div className='header__logo'>
+              <img src='/img/logo-white.png' alt='logo' />
+            </div>
+          </Col>
+          <Col>
+            <nav className='nav nav--user'>
+              {loginSignupMarkup}
+            </nav>
+          </Col>
+        </Row>
+      </Container>
     </header>
   )
 };
