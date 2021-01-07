a9ec02b882727dc9cbf687cdb0908b53043fcba2
Alan Fernandez <alan.f@msn.com>
2021-01-07 20:36:59 -0300

    Updated axios config

--- src/axios/index.js
+++ src/axios/index.js
@@ -2,6 +2,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: `${process.env.REACT_APP_API_URL}/api/v1/`,
+    baseURL: `https://gearwaves-api.herokuapp.com/api/v1/`,
     withCredentials: true,
 });
