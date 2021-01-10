cbfd28af118872035225960bde53e11b28dbde37
Alan Fernandez <alan.f@msn.com>
2021-01-10 17:34:34 -0300

    Spinning for loading instance in home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 // Components
-import ProductCard from "../components/ProductCard";
+import ProductCard from '../components/ProductCard';
 import { Container, Row, Col, Spinner } from 'reactstrap';
 import { showAlert } from '../util/alerts';
 // Redux
@@ -8,8 +8,7 @@ import { connect } from 'react-redux';
 // Actions
 import { getProducts } from '../redux/actions/dataActions';
 
-const Home = props => {
-
+const Home = (props) => {
   const { getProducts, loading } = props;
 
   useEffect(() => {
@@ -19,33 +18,49 @@ const Home = props => {
     if (!props.data.fetchedProducts) fetchData();
     if (props.match.path === '/success' && props.user.authenticated) {
       console.log('success');
-      showAlert('success', `Success!, check "My Gear" section in your account`, 5);
+      showAlert(
+        'success',
+        `Success!, check "My Gear" section in your account`,
+        5
+      );
       window.setTimeout(() => {
         props.history.push('/');
       }, 1500);
     }
-  }, [props.history, props.match.path, getProducts, props.data.fetchedProducts, props.user.authenticated])
+  }, [
+    props.history,
+    props.match.path,
+    getProducts,
+    props.data.fetchedProducts,
+    props.user.authenticated,
+  ]);
 
   let productsMap = props.data.products.map((product) => (
-    <Col key={product._id}><ProductCard product={product} key={product._id} /></Col>
+    <Col key={product._id}>
+      <ProductCard product={product} key={product._id} />
+    </Col>
   ));
 
-  let productsMarkup = !loading ? productsMap : <div className="spinner--container"><Spinner className="spinner" size="md" color="dark" /></div>;
+  let productsMarkup = !props.data.loading ? (
+    <div className="card--container">
+      <Row xs="1" md="2" lg="3">
+        {productsMap}
+      </Row>
+    </div>
+  ) : (
+    <div className="spinner--container">
+      <Spinner className="spinner-home" size="lg" type="grow" color="secondary" />
+    </div>
+  );
 
   return (
     <main className="main">
-      <Container>
-        <div className="card--container">
-          <Row xs="1" md="2" lg="3">
-          {productsMarkup}
-          </Row>
-        </div>
-      </Container>
+      <Container>{productsMarkup}</Container>
     </main>
   );
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   data: state.data,
   user: state.user,
 });
